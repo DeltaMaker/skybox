7f455552030ac1586b09eda39eabe8fa24439dfb
DeltaMaker <info@deltamaker.com>
2023-04-21 19:16:27 -0400

    display Ethernet MAC address

--- network-info/netinfo.py
+++ network-info/netinfo.py
@@ -20,17 +20,19 @@ def run_command(cmd_line):
 @app.route("/")
 def main():
     eth0_cmd = "/sbin/ifconfig eth0 | grep 'inet ' | awk '{ print $2}'"
+    eth0_cmd2 = "/sbin/ifconfig eth0 | grep 'ether ' | awk '{ print $2}'"
     wlan0_cmd = "/sbin/ifconfig wlan0 | grep 'inet ' | awk '{ print $2}'"
     hostname_cmd = "hostname"
 
     ethernet_str = run_command(eth0_cmd)
+    ethermac_str = run_command(eth0_cmd2)
     wifi_str = run_command(wlan0_cmd)
     hostname_str = run_command(hostname_cmd) + '.local'
 
-    print ("output=", ethernet_str, wifi_str, hostname_str)
+    print ("output=", ethernet_str, ethermac_str, wifi_str, hostname_str)
     return render_template('main.html',title="Network Info Page", 
-        ethernet=ethernet_str, wifi=wifi_str, hostname=hostname_str)
+        ethernet=ethernet_str, ethermac=ethermac_str, 
+        wifi=wifi_str, hostname=hostname_str)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port)
-
